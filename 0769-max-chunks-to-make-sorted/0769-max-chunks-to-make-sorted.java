254664c7de8cc9de6d70bae7d5bad9d612f84990
Pawan Kumar <pawankumarpal85@gmail.com>
2024-12-19 20:27:51 +0530

    Time: 1 ms (100.00%), Space: 41.3 MB (15.09%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
@@ -1,5 +1,30 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
+                int n = arr.length;
+        int[] sortedArr = arr.clone();
+        Arrays.sort(sortedArr);
+
+        // Maps to track frequency of elements in original and sorted arrays
+        Map<Integer, Integer> freqOriginal = new HashMap<>();
+        Map<Integer, Integer> freqSorted = new HashMap<>();
+
+        int chunks = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Update frequency in original array
+            freqOriginal.put(arr[i], freqOriginal.getOrDefault(arr[i], 0) + 1);
+
+            // Update frequency in sorted array
+            freqSorted.put(sortedArr[i], freqSorted.getOrDefault(sortedArr[i], 0) + 1);
+
+            // If the two maps are equal, we found a valid chunk
+            if (freqOriginal.equals(freqSorted)) {
+                chunks++;
+            }
+        }
+
+        return chunks;
+        /**
 		int chunks=0;
 		int max=0;
 		
@@ -9,6 +34,6 @@ class Solution {
 				chunks++;
 			}
 		}
-        return chunks;
+        return chunks;*/
     }
 }
